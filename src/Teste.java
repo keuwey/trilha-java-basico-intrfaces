8493e82b2b8034243fc60935cc78f031c9c95c0d
keuwey <keuwey@protonmail.com>
2024-05-15 18:57:25 -0300

    Corrigindo iPhone e adicionando classe de Teste

---
+++ src/Teste.java
@@ -0,0 +1,15 @@
+public class Teste extends iPhone {
+    public static void main(String[] args) {
+        iPhone dispositivo = new iPhone();
+        dispositivo.selecionarMusica();
+        dispositivo.tocar();
+        dispositivo.atender();
+        dispositivo.adicionarNovaAba();
+        dispositivo.atualizarPagina();
+        dispositivo.exibirPagina();
+        dispositivo.ligar();
+        dispositivo.pausar();
+        dispositivo.iniciarCorreioDeVoz();
+
+    }
+}
--- src/iPhone.java
+++ src/iPhone.java
@@ -1,49 +1,47 @@
 public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorDeInternet {
-    public static void main(String[] args) {
 
-        @Override
-        public void ligar () {
-            System.out.println("Ligando...");
-        }
+    @Override
+    public void ligar() {
+        System.out.println("Ligando...");
+    }
 
-        @Override
-        public void atender () {
-            System.out.println("Atendendo");
-        }
+    @Override
+    public void atender() {
+        System.out.println("Atendendo");
+    }
 
-        @Override
-        public void iniciarCorreioDeVoz () {
-            System.out.println("Iniciando correio de voz");
-        }
+    @Override
+    public void iniciarCorreioDeVoz() {
+        System.out.println("Iniciando correio de voz");
+    }
 
-        @Override
-        public void tocar () {
-            System.out.println("Tocando música");
-        }
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música");
+    }
 
-        @Override
-        public void pausar () {
-            System.out.println("Música pausada...");
-        }
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada...");
+    }
 
-        @Override
-        public void selecionarMusica () {
-            System.out.println("Selecionando música...");
-        }
+    @Override
+    public void selecionarMusica() {
+        System.out.println("Selecionando música...");
+    }
 
-        @Override
-        public void exibirPagina () {
-            System.out.println("Exibindo página...");
-        }
+    @Override
+    public void exibirPagina() {
+        System.out.println("Exibindo página...");
+    }
 
-        @Override
-        public void adicionarNovaAba () {
-            System.out.println("Adicionando nova aba...");
-        }
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba...");
+    }
 
-        @Override
-        public void atualizarPagina () {
-            System.out.println("Atualizando página...");
-        }
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página...");
     }
 }
